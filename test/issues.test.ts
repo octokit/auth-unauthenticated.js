1c2afc4cdd814cccbfeda7b2d298a0339f493500
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-10-28 11:44:52 -0700

    test: do not prevent `POST /app-manifests/{code}/conversions` requests

---
+++ test/issues.test.ts
@@ -0,0 +1,25 @@
+import { request } from "@octokit/request";
+import fetchMock, { MockMatcherFunction } from "fetch-mock";
+
+import { createUnauthenticatedAuth } from "../src/index";
+
+test('https://github.com/octokit/auth-unauthenticated.js/issues/29', async () => {
+  const requestMock = request.defaults({
+    headers: {
+      "user-agent": "test",
+    },
+    request: {
+      fetch: fetchMock.sandbox().postOnce('path:/app-manifests/123/conversions', { 
+        status: 201,
+        body: { id: 1}
+       }),
+    },
+  });
+
+  const { hook } = createUnauthenticatedAuth({ reason: "test" });
+  const { data } = await hook(requestMock, "POST /app-manifests/{code}/conversions", {
+    code: 123
+  });
+
+  expect(data).toStrictEqual({ id: 1 });
+});
